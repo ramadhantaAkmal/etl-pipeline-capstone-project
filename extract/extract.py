b23f75c1561cbe2b7409904a671d6d9b05d2a7f6
ramadhantaAkmal <ramadhanta0412@gmail.com>
2025-09-15 23:33:49 +0700

    feat: implement simple extraction
    temporary extract process will change to more complex and dynamic

--- extract/extract.py
+++ extract/extract.py
@@ -1,12 +1,12 @@
 import pandas as pd
     
-def extract_csv(csv_path) -> pd.DataFrame :
-    return pd.read_csv(csv_path,skiprows=4)
+def extract_csv(path,skiprows) -> pd.DataFrame :
+    return pd.read_csv(path,skiprows=skiprows)
 
 def extract_json(json_path) -> pd.DataFrame:
     return pd.read_json(json_path)
     
-def extract_data( csv_path, json_path) -> dict:  
-    csv_df = extract_csv(csv_path)
-    json_df = extract_json(json_path)
-    return {"csv_data": csv_df, "json_data": json_df}
+# def extract_data( csv_path, json_path) -> dict:  
+#     csv_df = extract_csv(csv_path)
+#     json_df = extract_json(json_path)
+#     return {"csv_data": csv_df, "json_data": json_df}
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from config.settings import KAGGLE_PATH,EXTRACT_PATH
 from extract.fetch_data import fetch_dataset
+from extract.extract import extract_csv,extract_json
 
 if __name__ == "__main__":
     fetch_dataset(kaggle_path=KAGGLE_PATH, extract_path=EXTRACT_PATH)
+    
+    electricity_access_percent = extract_csv(f'{EXTRACT_PATH}/electricity_access_percent.csv', skiprows=4)
+    gdp_data = extract_csv(f'{EXTRACT_PATH}/gdp_data.csv', skiprows=4)
+    population_data = extract_csv(f'{EXTRACT_PATH}/population_data.csv', skiprows=4)
+    rural_population_percent = extract_csv(f'{EXTRACT_PATH}/rural_population_percent.csv', skiprows=4)
+    
+    print(gdp_data)
+    
+    
