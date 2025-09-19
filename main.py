9116527aea595addd1fec76dcd34699f4833d17f
ramadhantaAkmal <ramadhanta0412@gmail.com>
2025-09-19 17:33:58 +0700

    feat: created merge df algorithm

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from config.settings import KAGGLE_PATH,EXTRACT_PATH
 from extract.fetch_data import fetch_dataset
 from extract.extract import extract_data_to_df
+from transform.transform import to_long_format,merge_df_list
 
 if __name__ == "__main__":
-    key_columns = ["Country Name","Country Code","Indicator Name","Indicator Code"]
+    #Extract data
+    key_columns_extract = ["Country Name","Country Code","Indicator Name","Indicator Code"]
     
     fetch_dataset(kaggle_path=KAGGLE_PATH, extract_path=EXTRACT_PATH)
     
-    df_list = extract_data_to_df(EXTRACT_PATH,key_columns,4)
+    df_list = extract_data_to_df(EXTRACT_PATH,key_columns_extract,4)
     
-    print(df_list)
+    transformed_df_list = list(map(to_long_format,df_list))
+    
+    #Transform data
+    key_columns_transform = ['Country_Code','Country_Name', 'Year']
+    merged_df = merge_df_list(transformed_df_list, key_columns=key_columns_transform, how='inner')
+    print(merged_df)
+    
+    #Load data
+    
     
     
--- transform/transform.py
+++ transform/transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def to_long_format(df:pd.DataFrame) -> pd.DataFrame:
+    df.drop('Unnamed: 62', axis=1, inplace=True)
     value_name = f'{df['Indicator Name'][0]}({df['Indicator Code'][0]})'
     dt = df.drop(columns=['Indicator Name', 'Indicator Code'])
     dt['Country Name'] = dt['Country Name'].str.strip() 
@@ -9,3 +10,16 @@ def to_long_format(df:pd.DataFrame) -> pd.DataFrame:
                    var_name='Year', value_name=value_name)
     dt_long['Year'] = dt_long['Year'].astype(int)
     return dt_long
+
+def merge_df_list(df_list, key_columns:list[str], how:str) -> pd.DataFrame:
+    if len(df_list)==1:
+        return df_list[0]
+    x = []
+    if len(df_list)%2 != 0:
+        x.append(df_list.pop(-1))
+    
+    for i in range(0,len(df_list),2):
+        merger = pd.merge(df_list[i], df_list[i+1], on=key_columns, how='inner')
+        x.append(merger)
+    return merge_df_list(x,key_columns,how)
+    
